0ca95fa90bd51afaa3316d2454c14057da5abd64
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-21 10:27:14 +0100

    Add exports.swift with @_exported re-exports
    Re-export all semantic dependencies for consumers.

---
+++ Sources/Pool Primitives/exports.swift
@@ -0,0 +1,11 @@
+// exports.swift
+// Re-export dependencies for consumers.
+
+@_exported import Async_Primitives
+@_exported import Buffer_Primitives
+@_exported import Array_Primitives
+@_exported import Dimension_Primitives
+@_exported import Reference_Primitives
+@_exported import Effect_Primitives
+@_exported import Index_Primitives
+@_exported import Collection_Primitives
